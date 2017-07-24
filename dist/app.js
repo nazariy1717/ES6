558a18a4c8bf433353998affe3bc892c2b3c3aea
nazariy17 <nazariy2410@gmail.com>
2017-07-24 12:14:22 +0300

    let && const

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,22 @@
 'use strict';
 
-var a = 'dasd';
+var buttons = document.querySelectorAll('button');
+
+var _loop = function _loop(i) {
+    button = buttons[i];
+
+    button.innerText = i;
+    button.onclick = function (e) {
+        console.log(i);
+    };
+};
+
+for (var i = 0; i < buttons.length; i++) {
+    var button;
+
+    _loop(i);
+}
+
+var MATH = {
+    PI: 3.14159
+};
--- src/app.js
+++ src/app.js
@@ -1,2 +1,15 @@
 
-let a = 'dasd';
+var buttons = document.querySelectorAll('button');
+
+for( let i = 0; i < buttons.length; i++){
+    var button = buttons[i];
+    button.innerText = i;
+    button.onclick = function(e){
+      console.log(i);
+    };
+}
+
+const MATH = {
+    PI: 3.14159
+};
+
